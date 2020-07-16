8c114502908ae46eb7308b7bb539c1d00301175b
Erdem Camlioglu <secamlioglu@gmail.com>
2020-07-16 22:00:57 +0300

    Connectors restored

--- src/busautomation/database.java
+++ src/busautomation/database.java
@@ -15,7 +15,7 @@ package busautomation;
 public class database {
     public static final String user_id = "root";
     public static final String password = "";
-    public static final String db_name = "trial"; 
+    public static final String db_name = "odev"; 
     public static final String host =  "localhost";
     public static final int port = 3306;
 }
